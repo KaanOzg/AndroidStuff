ad4b659b070ccce6c054008d58dac4dabd78e5f6
KaanOzg <kaanozg0@gmail.com>
2015-05-08 22:31:30 -0700

    MyFlickrApp

---
+++ MyFlickr/app/src/main/java/com/detroitteatime/myflickr/Constants.java
@@ -0,0 +1,20 @@
+package com.detroitteatime.myflickr;
+
+/**
+ * Created by mark on 4/29/15.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "190cb79651508c257f838c458513effc";
+    public static final String SECRET = "93d88f46e878686f";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
